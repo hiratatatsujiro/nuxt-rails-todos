e054b08e9e4d8ccbc737abe9b6015cafd69947ea
平田達二郎 <hiratatoorujirou@hiratatoorujirounoMacBook-Pro.local>
2022-06-14 04:29:36 +0900

    association記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :todos, only: [ :index, :create, :destroy]
+    resources :todos, only: [:index, :create, :destroy]
+    # -------- ここから追記 --------
+    resources :users, only: [:index, :create]
+  # -------- ここまで追記 --------
   end
 end
